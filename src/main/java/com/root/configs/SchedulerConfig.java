88a4e5a846aff74822c8ef8fc754ebd93bfe7d72
weixiaojun-ludi <835351672@qq.com>
2020-02-08 09:12:15 +0800

    002

--- src/main/java/com/root/configs/SchedulerConfig.java
+++ src/main/java/com/root/configs/SchedulerConfig.java
@@ -16,7 +16,7 @@ public class SchedulerConfig  {
     public SchedulerFactoryBean schedulerFactoryBean() throws IOException {
         //获取配置属性
         PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
-        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz.properties"));
+        propertiesFactoryBean.setLocation(new ClassPathResource("/quartz2.properties"));
         //在quartz.properties中的属性被读取并注入后再初始化对象
         propertiesFactoryBean.afterPropertiesSet();
         //创建SchedulerFactoryBean
